26866cc8d2df544b3135dea53596e920cd376b45
Andrey Kirkicha <kirkicha.andrey@mail.ru>
2023-07-24 17:39:16 +0300

    Redifinition of operator

--- 5_2/string.cpp
+++ 5_2/string.cpp
@@ -38,14 +38,14 @@ struct String
     {
         if (this != &other) 
         {
-			delete [] this->str;
-			this->size = other.size;
-			size_t size = other.size;
-			this->str = new char[size + 1];
+            delete [] this->str;
+            this->size = other.size;
+            size_t size = other.size;
+            this->str = new char[size + 1];
 
-			for (int i = 0; i < size; i++) { (this->str)[i] = other.str[i]; }
+            for (int i = 0; i < size; i++) { (this->str)[i] = other.str[i]; }
 
-			(this->str)[size] = '\0';
+            (this->str)[size] = '\0';
 		}
 
 		return *this;
