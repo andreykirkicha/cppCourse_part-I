ab1dd6decc6e4003a5c85dbd81036d569369f853
Andrey Kirkicha <kirkicha.andrey@mail.ru>
2023-08-04 19:37:51 +0300

    Function templates

---
+++ 6_2/copy_n.cpp
@@ -0,0 +1,32 @@
+#include <cstddef>
+#include <iostream>
+
+/*
+    Arguments of function "copy_n":
+        1.  Destination array
+        2.  Source array
+        3.  Number of elements that will be copied
+*/
+
+template <typename Type_1, typename Type_2>
+void copy_n(Type_1 *dest, Type_2 *src, size_t n)
+{
+    for (size_t i = 0; i < n; ++i) { dest[i] = static_cast<Type_1>(src[i]); }
+}
+
+//========================================================================================================
+
+int main()
+{
+    int ints[] = {1, 2, 3, 4};
+    double doubles[4] = {};
+
+    for (size_t i = 0; i < 4; ++i) { std::cout << ints[i] << ", " << doubles[i] << std::endl; }
+
+    copy_n(doubles, ints, 4);
+
+    std::cout << std::endl;
+    for (size_t i = 0; i < 4; ++i) { std::cout << ints[i] << ", " << doubles[i] << std::endl; }
+
+    return 0;
+}
